6d08db4ccfabdabaaacebf686430f6b620ad9918
1578482035 <43672700+1578482035@users.noreply.github.com>
2018-09-28 22:04:04 +0800

    Add files via upload

---
+++ qs.js
@@ -0,0 +1,84 @@
+//第一张图片点击函数
+function gaoyue() {
+  var i1 = document.getElementById("tp1");
+  i1.style.display = "block";
+  i1.style.animation = "dong1 1s"; //图片透明度动画
+  li1.style.color = "#f4291c";
+  li2.style.color = "#c8c2c2";
+  li3.style.color = "#c8c2c2";
+  li4.style.color = "#c8c2c2";
+  li5.style.color = "#c8c2c2";
+  tp2.style.display = "none";
+  tp3.style.display = "none";
+  tp4.style.display = "none";
+  tp5.style.display = "none";
+
+}
+
+//第二张图片点击函数
+function tianming() {
+  var i2 = document.getElementById("tp2");
+  i2.style.display = "block";
+  i2.style.animation = "dong1 1s";
+  li2.style.color = "#f4291c";
+  li3.style.color = "#c8c2c2";
+  li4.style.color = "#c8c2c2";
+  li5.style.color = "#c8c2c2";
+  li1.style.color = "#c8c2c2";
+  tp3.style.display = "none";
+  tp4.style.display = "none";
+  tp5.style.display = "none";
+  tp1.style.display = "none";
+
+}
+
+//第三章图片点击函数
+function shilan() {
+  var i3 = document.getElementById("tp3");
+  i3.style.display = "block";
+  i3.style.animation = "dong1 1s";
+  li3.style.color = "#f4291c";
+  li4.style.color = "#c8c2c2";
+  li5.style.color = "#c8c2c2";
+  li1.style.color = "#c8c2c2";
+  li2.style.color = "#c8c2c2";
+  tp4.style.display = "none";
+  tp5.style.display = "none";
+  tp1.style.display = "none";
+  tp2.style.display = "none";
+
+}
+
+//第四章图片点击函数
+function shaoyu() {
+  var i4 = document.getElementById("tp4");
+  i4.style.display = "block";
+  i4.style.animation = "dong1 1s";
+  li4.style.color = "#f4291c";
+  li5.style.color = "#c8c2c2";
+  li1.style.color = "#c8c2c2";
+  li2.style.color = "#c8c2c2";
+  li3.style.color = "#c8c2c2";
+  tp5.style.display = "none";
+  tp1.style.display = "none";
+  tp2.style.display = "none";
+  tp3.style.display = "none";
+
+}
+
+//第五章图片点击函数
+function sanshigong() {
+  var i5 = document.getElementById("tp5");
+  i5.style.display = "block";
+  i5.style.animation = "dong1 1s";
+  li5.style.color = "#f4291c";
+  li1.style.color = "#c8c2c2";
+  li2.style.color = "#c8c2c2";
+  li3.style.color = "#c8c2c2";
+  li4.style.color = "#c8c2c2";
+  tp1.style.display = "none";
+  tp2.style.display = "none";
+  tp3.style.display = "none";
+  tp4.style.display = "none";
+
+}
